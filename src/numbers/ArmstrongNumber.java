c211bfd2a8e89ed96c8114070048d2184d7e00d1
grss1228 <grss1228@gmail.com>
2022-12-20 17:42:29 +0530

    Armstrong Number

---
+++ src/numbers/ArmstrongNumber.java
@@ -0,0 +1,27 @@
+package numbers;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("*****ARMSTRONG NUMBER CHECKING*****");
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        double sum=0;
+        int temp = num;
+        while(temp>0){
+            int rem = temp%10;
+            sum = sum+Math.pow(rem,3);
+            temp=temp/10;
+        }
+        if(sum==num){
+            System.out.println(num+" is an armstrong number");
+        }
+        else{
+            System.out.println("Not an Armstrong Number");
+        }
+    }
+}

95714d097b0834e3edc933eae89f36cb082b5217
grss1228 <grss1228@gmail.com>
2022-12-22 09:58:59 +0530

    for each

---
+++ src/Arrays/PrintArrayUsingForEach.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class PrintArrayUsingForEach {
+    public static void main(String[] args) {
+        int[] marks = {55,65,76,67,89,45,34,56,87};
+
+        for (int item:marks){
+            System.out.println(item);
+        }
+    }
+}

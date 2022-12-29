5862276f5c331cb073cca6bec2a37ef66c671290
grss1228 <grss1228@gmail.com>
2022-12-29 17:23:22 +0530

    Pattern 5

--- src/patterns/Pattern5.java
+++ src/patterns/Pattern5.java
@@ -14,9 +14,16 @@ public class Pattern5 {
     public static void main(String[] args) {
         int n = 5;
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(j+" ");
+        for(int row=0;row<2*n;row++){
+            int colsInRow;
+            if(row>n){
+                colsInRow = 2*n-row;
+            }
+            else{
+                colsInRow = row;
+            }
+            for(int col=1;col<=colsInRow;col++){
+                System.out.print("* ");
             }
             System.out.println();
         }

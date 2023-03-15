adafaf8d9aa4d2935a8ff6b64869abe98d711203
GautamRaj-12 <gautamraj996@gmail.com>
2023-03-15 06:38:44 +0530

    Binary Search and Binary Search Agnostic

---
+++ src/Searching/BinarySearch.java
@@ -0,0 +1,34 @@
+package Searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-18,-16,0,1,2,23,34,56,78};
+        System.out.println("The provided array is: "+ Arrays.toString(arr));
+        System.out.println("Enter the value to be searched: ");
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+        binarySearch(arr,target);
+    }
+    static void binarySearch(int arr[],int target){
+        int start=0,end=arr.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]==target){
+                System.out.println("Element is found at index "+mid);
+                break;
+            }
+            else if(arr[mid]>target){
+                end = mid-1;
+            }
+            else if(arr[mid]<target){
+                start = mid+1;
+            }
+        }
+        if(start>end){
+            System.out.println("Element is not present in the array.");
+        }
+    }
+}

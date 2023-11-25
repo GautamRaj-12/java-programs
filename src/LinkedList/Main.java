0f834239420698c804a7baa92af5b7054ce142c6
Gautam Raj <gautamraj996@gmail.com>
2023-11-25 10:13:47 +0530

    Singly Linked List Implementation: Insertion at start and displaying the list

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.insertFirst(3);
+        list.insertFirst(5);
+        list.insertFirst(2);
+
+        list.display();
+    }
+}
